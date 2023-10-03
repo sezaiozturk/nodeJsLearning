9b74b94b5fdf9bcab5ee2194980a80ea150b07b2
Sezai Ozturk <ozturksezai52@gmail.com>
2023-10-03 16:32:17 +0300

    Feature:Node js prime numbers

---
+++ first.js
@@ -0,0 +1,15 @@
+const arguments = process.argv.slice(2);
+const showPrimeNumbers = (lowNumber, hightNumber) => {
+  for (let i = lowNumber; i <= hightNumber; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+      }
+    }
+    if (isPrime) {
+      console.log(i);
+    }
+  }
+};
+showPrimeNumbers(arguments[0] * 1, arguments[1] * 1);
