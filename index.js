b94c304ed4bd882ae80fdc02797315c703c05915
Sezai Ozturk <ozturksezai52@gmail.com>
2023-10-21 13:46:00 +0300

    Feature:middleware

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("middleware 1");
+  next();
+});
+
 app.use((req, res) => {
-  res.end("hello nodemon");
+  console.log("middleware 3");
+  res.send("<h1>Middleware</h1>");
 });
 app.listen(3000, () => {
   console.log("listening on port 3000");
